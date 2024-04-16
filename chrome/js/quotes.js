467352f06c9f4ea209b37417986410c6234347e8
hyoni <jih70004@gmail.com>
2024-04-16 14:24:21 +0900

    [mod] 변수 상수로 수정

--- chrome/js/quotes.js
+++ chrome/js/quotes.js
@@ -53,7 +53,7 @@ const quote = document.querySelector('#quote span:first-child');
 const author = document.querySelector('#quote span:last-child');
 
 
-let todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
+const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
 quote.innerText = todayQuote.quote;
 author.innerText = todayQuote.author;
 
