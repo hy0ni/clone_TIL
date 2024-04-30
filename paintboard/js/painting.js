d635a1638e33658b6cc79e951727a6a002fb62c3
hyoni <jih70004@gmail.com>
2024-05-01 03:58:17 +0900

    [add] line width 수정 가능한 input 추가

--- paintboard/js/painting.js
+++ paintboard/js/painting.js
@@ -1,9 +1,11 @@
+const $lineWidth = document.getElementById('line-width');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = $lineWidth.value;
 let isPainting = false;
+
 function onMove(e) {
   if (isPainting) {
     ctx.lineTo(e.offsetX, e.offsetY);
@@ -12,16 +14,20 @@ function onMove(e) {
   }
   ctx.moveTo(e.offsetX, e.offsetY);
 }
-
 function startPainting() {
   isPainting = true;
 }
 function cancelPainting() {
   isPainting = false;
-
+  ctx.beginPath();
 }
+function onLineWidthChange(e) {
+  ctx.lineWidth = e.target.value;
+}
+
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
 canvas.addEventListener('mouseleave', cancelPainting);
-// document.addEventListener('mouseup', onMouseUp);
+
+$lineWidth.addEventListener('change', onLineWidthChange);
--- paintboard/paintboard.md
+++ paintboard/paintboard.md
@@ -48,3 +48,39 @@ canvas.addEventListener("mouseleave", onMouseUp);
    document에 mouseup event를 주게되면 어디에서든지 마우스에서 손을 떼면 isPainting은 false가 된다.
 
 해당 코드에서는 첫번째 방법으로 해결함.
+
+### 2. Modify line thickness
+
+그림판에 선의 굵기를 수정할 수 있는 input 추가하기.
+
+```html
+<input type="range" id="line-width" min="1" max="10" value="5" />
+```
+
+최소값/최대값/초기값
+
+```javascript
+function onLineWidthChange(e) {
+  ctx.lineWidth = e.target.value;
+}
+$lineWidth.addEventListener("change", onLineWidthChange);
+```
+
+선을 그릴때 input의 value값으로 그려주는데 이 경우 새로 그려지는 선들도 value를 변경할 때마다 같이 변경되므로,   
+이전에 그려진 선과 새로운 선의 연결을 끊어줘야한다.
+(모든 line들은 같은 path로 그려지기 때문이다.)
+
+```javascript
+function cancelPainting() {
+  isPainting = false;
+  ctx.beginPath();
+}
+```
+
+user가 선을 그리고 그리는게 끝나면, 새로운 path를 시작하도록 해야한다.
+
+```html
+<input type="range" id="line-width" min="1" max="10" value="5" step="0.5" />
+```
+
+range input의 또 다른 attribute인 step을 지정해주면 증가되는 정도의 값을 지정해줄 수 있다.
